e547ab777291a5f5f2ca22c2924bbe0a52184f8e
choomba <beginanon@gmail.com>
2023-07-02 21:03:37 -0700

    server: Add /interrupt endpoint to stop generation
    Previously, a stream request was not cancellable, which blocked
    subsequent calls.
    
    Now, we can interrupt to cut the stream short.

--- examples/server/server.cpp
+++ examples/server/server.cpp
@@ -122,6 +122,7 @@ struct llama_server_context {
     bool stopped_eos = false;
     bool stopped_word = false;
     bool stopped_limit = false;
+    bool stopped_req = false;
     std::string stopping_word;
     int32_t multibyte_pending = 0;
 
@@ -141,6 +142,7 @@ struct llama_server_context {
         stopped_eos = false;
         stopped_word = false;
         stopped_limit = false;
+        stopped_req = false;
         stopping_word = "";
         multibyte_pending = 0;
 
@@ -214,6 +216,10 @@ struct llama_server_context {
         // number of tokens to keep when resetting context
         n_remain = params.n_predict;
         llama_set_rng_seed(ctx, params.seed);
+    }
+
+    void stopCompletion() {
+        stopped_req = true;
     }
 
     llama_token nextToken() {
@@ -812,6 +818,10 @@ int main(int argc, char ** argv) {
         res.set_content("<h1>llama.cpp server works</h1>", "text/html");
     });
 
+    svr.Post("/interrupt", [&llama](const Request & req, Response & res) {
+        llama.stopCompletion();
+    });
+
     svr.Post("/completion", [&llama](const Request & req, Response & res) {
         llama.rewind();
         llama_reset_timings(llama.ctx);
@@ -849,7 +859,7 @@ int main(int argc, char ** argv) {
             const auto chunked_content_provider = [&](size_t, DataSink & sink) {
                 size_t sent_count = 0;
 
-                while (llama.has_next_token) {
+                while (llama.has_next_token && !llama.stopped_req) {
                     const std::string token_text = llama.doCompletion();
                     if (llama.multibyte_pending > 0) {
                         continue;
